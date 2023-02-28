0814eb907d972700bf30f2fe0daf6ba147d04efa
pippin985 <pepijnkokken@live.nl>
2023-02-28 04:36:18 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server to get bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codexo.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
